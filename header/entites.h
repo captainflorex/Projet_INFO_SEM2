3219915db5b8eda1580654d90b0a8939315b7de4
remimtd <remi.matteoda@edu.ece.fr>
2026-03-31 23:32:16 +0200

    Update entites.h

--- header/entites.h
+++ header/entites.h
@@ -1,6 +1,8 @@
 #ifndef ENTITES_H
 #define ENTITES_H
 
+#include "eclair.h"
+
 #define LARGEUR_FENETRE  800
 #define HAUTEUR_FENETRE  600
 #define HAUTEUR_INFO      60
@@ -8,8 +10,13 @@
 
 #define MAX_BULLES      30
 #define MAX_PROJECTILES 10
-#define MAX_ARMES       5
+#define MAX_BONUS       5
 #define PSEUDO_LEN      32
+
+#define NB_TYPES_BONUS 3
+#define DUREE_ARME_SPECIALE 10.0f
+
+
 
 typedef enum {
     BULLE_GRANDE = 0,
@@ -20,8 +27,8 @@ typedef enum {
 
 typedef enum {
     ARME_BASIQUE = 0,
-    ARME_RAPIDE,
-    ARME_MULTIPLE,
+    ARME_TIR_RAPIDE,
+    ARME_TIR_TRIPLE,
     ARME_EXPLOSION
 } TypeArme;
 
@@ -46,14 +53,16 @@ typedef struct {
 
 typedef struct {
     float x, y;
-    float vy;
-    int   active;
+    float vx, vy;
+    int active;
+    int explosion;
 } Projectile;
 
 typedef struct {
     float    x, y;
     TypeArme type;
-    int      active;
+    int active;
+    float timer;
 } BonusArme;
 
 typedef struct {
@@ -68,13 +77,19 @@ typedef struct {
     Bulle       bulles[MAX_BULLES];
     int         nb_bulles;
     Projectile  projectiles[MAX_PROJECTILES];
-    BonusArme   bonus[MAX_ARMES];
+    BonusArme   bonus[MAX_BONUS];
     Boss        boss;
     int         niveau;
     float       temps_restant;
     int         decompte;
     float decompte_timer;
+    ListeEclairs eclairs;
+    int bulles_en_attente;   /* nb de bulles à spawner après le décompte */
+    float timer_arme_speciale;
+    float explosion_x, explosion_y;
+    float explosion_timer;   /* > 0 = explosion en cours */
 } EtatJeu;
+
 
 float rayon_bulle(TailleBulle t);
 
